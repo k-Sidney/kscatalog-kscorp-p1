5d96f8984de6a9e75abd478c807ab92c9cc13b9e
k-Sidney <kleubersidney@hotmail.com>
2023-11-28 17:39:28 -0300

    Backend project created

---
+++ BackEnd/src/main/java/com/kscorp/kscatalog/KscatalogApplication.java
@@ -0,0 +1,13 @@
+package com.kscorp.kscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KscatalogApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/kscorp/kscatalog/KscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kscorp.kscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
