dfadcd05a73d7a23f74d4d6330c529eb3f3207d3
k-Sidney <kleubersidney@hotmail.com>
2023-11-30 14:26:07 -0300

    DTO

--- BackEnd/src/main/java/com/kscorp/kscatalog/resources/CategoryResource.java
+++ BackEnd/src/main/java/com/kscorp/kscatalog/resources/CategoryResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kscorp.kscatalog.dto.CategoryDTO;
 import com.kscorp.kscatalog.entities.Category;
 import com.kscorp.kscatalog.services.CategoryService;
 
@@ -19,8 +20,8 @@ public class CategoryResource {
 	private CategoryService service;
 
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll(){
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 		
 	}
--- BackEnd/src/main/java/com/kscorp/kscatalog/services/CategoryService.java
+++ BackEnd/src/main/java/com/kscorp/kscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.kscorp.kscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.kscorp.kscatalog.dto.CategoryDTO;
 import com.kscorp.kscatalog.entities.Category;
 import com.kscorp.kscatalog.repositories.CategoryRepository;
 
@@ -16,8 +18,13 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		
+		return  list.stream()
+				.map(x -> new CategoryDTO(x))
+				.collect(Collectors.toList());
+		
 	}
 	
 }
