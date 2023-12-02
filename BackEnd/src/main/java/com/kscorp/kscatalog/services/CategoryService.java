6a9a79e30a89a54c080f8dd42a8c536aa9cb1fe8
k-Sidney <kleubersidney@hotmail.com>
2023-12-02 15:06:12 -0300

    Auditing data

--- BackEnd/src/main/java/com/kscorp/kscatalog/services/CategoryService.java
+++ BackEnd/src/main/java/com/kscorp/kscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.kscorp.kscatalog.dto.CategoryDTO;
 import com.kscorp.kscatalog.entities.Category;
 import com.kscorp.kscatalog.repositories.CategoryRepository;
+import com.kscorp.kscatalog.services.exceptions.DatabaseException;
 import com.kscorp.kscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -52,6 +55,17 @@ public class CategoryService {
 		}
 		catch(EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+	public void delete(Long id) {
+		try {
+	repository.deleteById(id);	
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Interity violation");
 		}
 	}
 	
