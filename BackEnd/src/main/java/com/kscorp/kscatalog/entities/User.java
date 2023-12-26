7520ba2bedccec399ec27843fd49791fc2cbe8c0
k-Sidney <kleubersidney@hotmail.com>
2023-12-26 20:16:45 -0300

    Resource Server

--- BackEnd/src/main/java/com/kscorp/kscatalog/entities/User.java
+++ BackEnd/src/main/java/com/kscorp/kscatalog/entities/User.java
@@ -101,12 +101,10 @@ public class User implements UserDetails, Serializable {
 		return roles;
 	}
 
+	
+
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null)?
-		0 : id.hashCode());
 		return Objects.hash(id);
 	}
 
@@ -119,12 +117,7 @@ public class User implements UserDetails, Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		if(id == null) {
-			if(other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
+		return Objects.equals(id, other.id);
 	}
 
 	@Override
