a063de85ab6bfb575e12e773b07531ba2c4fe9d3
k-Sidney <kleubersidney@hotmail.com>
2024-01-21 20:04:13 -0300

    Navbar styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,9 +1,13 @@
-import './assets/styles/custom.scss';
-import './App.css';
+import Navbar from "./components/Navbar";
+import "./assets/styles/custom.scss";
+import "./App.css";
 
 function App() {
   return (
-    <h1>Hello KSCatalog</h1>
+    <>
+      <Navbar />
+      <h1>Hello KSCatalog</h1>
+    </>
   );
 }
 
