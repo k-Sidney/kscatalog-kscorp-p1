bf0db27bca1aa338e85ffbd90a37298412d26a57
k-Sidney <kleubersidney@hotmail.com>
2024-01-25 03:02:07 -0300

    Routes

---
+++ frontweb/src/components/ProductCard/index.tsx
@@ -0,0 +1,21 @@
+import ProductPrice from "components/ProductPrice";
+import "./styles.css";
+
+import ProductImg from 'assets/images/product.png';
+
+const ProductCard = () => {
+  return (
+    <div className="base-card product-card">
+      <div className="card-top-container">
+        <img src={ProductImg} alt="Nome do Produto" />
+      </div>
+      <div className="card-bottom-container">
+        <h6>Nome do produto</h6>
+        <ProductPrice/>
+      </div>
+
+    </div>
+  )
+};
+
+export default ProductCard;
