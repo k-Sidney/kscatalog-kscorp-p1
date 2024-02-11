cf2e84a0b6cc0f39a2f1f70e047d4eeb2b0715ed
k-Sidney <kleubersidney@hotmail.com>
2024-02-11 16:46:32 -0300

    Routes, redirectins, nesting

--- frontweb/src/pages/Admin/index.tsx
+++ frontweb/src/pages/Admin/index.tsx
@@ -1,13 +1,26 @@
+import { Redirect, Route, Switch } from "react-router-dom";
 import Navbar from "./Navbar";
 
-import './styles.css';
+import "./styles.css";
 
 const Admin = () => {
   return (
     <div className="admin-container">
       <Navbar />
       <div className="admin-content">
-        <h1>Conteúdo</h1>
+        <Switch>
+          <Route path="/admin/products">
+            <h1>Product CRUD</h1>
+          </Route>
+
+          <Route path="/admin/categories">
+            <h1>Category CRUD</h1>
+          </Route>
+          <Redirect from="/admin" to="admin/products" exact/>
+          <Route path="/admin/users">
+            <h1>User CRUD</h1>
+          </Route>
+        </Switch>
       </div>
     </div>
   );
