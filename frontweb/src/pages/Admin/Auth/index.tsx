033c774c555daf72e643a7df09a6e418731a7f30
k-Sidney <kleubersidney@hotmail.com>
2024-02-16 16:36:01 -0300

    Banner styling

--- frontweb/src/pages/Admin/Auth/index.tsx
+++ frontweb/src/pages/Admin/Auth/index.tsx
@@ -1,11 +1,13 @@
 import { ReactComponent as AuthImage } from 'assets/images/auth-image.svg';
 import { Route, Switch } from 'react-router-dom';
 
+import './styles.css';
+
 const Auth = () => {
   return (
     <div className="auth-container">
       <div className="auth-banner-container">
-        <h1>Divulgue seus produtos no kscatalog</h1>
+        <h1>Divulgue seus produtos no KS Catalog</h1>
         <p>
           Faça parte do nosso catálogo de divulgação e aumente a venda dos seus
           produtos
