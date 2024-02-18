89068749ff49afe52fb555ead3d0891833c42b19
k-Sidney <kleubersidney@hotmail.com>
2024-02-18 16:09:38 -0300

    PrivateRoute

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,9 +1,9 @@
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
 import ButtonIcon from 'components/ButtonIcon';
 import { Link } from 'react-router-dom';
+import { isAuthenticated } from 'util/requests';
 
 import './styles.css';
-import { isAuthenticated } from 'util/requests';
 
 const Home = () => {
   return (
