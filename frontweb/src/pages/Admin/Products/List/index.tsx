65510a2476ebb85b1b86de4da18ff25fc86153d1
k-Sidney <kleubersidney@hotmail.com>
2024-03-07 20:29:15 -0300

    CategoryBadge

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -1,4 +1,4 @@
-import ProductCrudCard from 'components/ProductCrudCard';
+import ProductCrudCard from 'pages/Admin/Products/ProductCrudCard';
 
 import './styles.css';
 import { Link } from 'react-router-dom';
@@ -18,6 +18,11 @@ const List = () => {
         id: 2,
         name: 'Eletrônicos',
       },
+      {
+        id: 1,
+        name: 'Computadores',
+      },
+      
     ],
   };
   return (
