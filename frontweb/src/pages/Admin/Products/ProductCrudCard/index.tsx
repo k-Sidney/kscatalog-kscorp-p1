1f08a9111afa0179bb52533afa339238f397d700
k-Sidney <kleubersidney@hotmail.com>
2024-03-13 21:11:41 -0300

    CRUD routes

--- frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
+++ frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
@@ -3,6 +3,7 @@ import './styles.css';
 import ProductPrice from 'components/ProductPrice';
 import { Product } from 'types/product';
 import CategoryBadge from '../CategoryBadge';
+import { Link } from 'react-router-dom';
 
 type Props = {
   product: Product;
@@ -15,7 +16,7 @@ const ProductCrudCard = ({ product }: Props) => {
         <img src={product.imgUrl} alt={product.name} />
       </div>
 
-      <div className='product-crud-card-description'>
+      <div className="product-crud-card-description">
         <div className="product-crud-card-bottom-container">
           <h6>{product.name}</h6>
           <ProductPrice price={product.price} />
@@ -27,8 +28,14 @@ const ProductCrudCard = ({ product }: Props) => {
         </div>
       </div>
       <div className="product-crud-card-buttons-container">
-        <button className="btn btn-outline-danger product-crud-card-button product-crud-card-button-first">EXCLUIR</button>
-        <button className="btn btn-outline-secondary product-crud-card-button">EDITAR</button>
+        <button className="btn btn-outline-danger product-crud-card-button product-crud-card-button-first">
+          EXCLUIR
+        </button>
+        <Link to={`/admin/products/${product.id}`}>
+          <button className="btn btn-outline-secondary product-crud-card-button">
+            EDITAR
+          </button>
+        </Link>
       </div>
     </div>
   );
