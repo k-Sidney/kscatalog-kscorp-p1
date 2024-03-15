958229b0351369df30dbb49a0210ce7e46bc94b3
k-Sidney <kleubersidney@hotmail.com>
2024-03-14 23:50:52 -0300

    ProductFilter input name layout

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -8,6 +8,7 @@ import { requestBackend } from 'util/requests';
 import Pagination from 'components/Pagination';
 
 import './styles.css';
+import ProductFilter from 'components/ProductFilter';
 
 type ControlComponentsData = {
   activePage: number;
@@ -52,8 +53,7 @@ const List = () => {
             ADICIONAR
           </button>
         </Link>
-
-        <div className="base-card product-filter-container">Search bar</div>
+        <ProductFilter />
       </div>
       <div className="row ">
         {page?.content.map((product) => (
